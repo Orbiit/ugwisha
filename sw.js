d2d41e7a66618e0c6d1a7b5195d7cd0b1ce85cef
SheepTester <ussa.say@gmail.com>
2019-05-31 21:49:34 -0700

    push update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ugwisha-sw-v1558833138490';
+const CACHE_NAME = 'ugwisha-sw-v1559364546397';
 const BACKGROUND_CACHE_NAME = 'ugwisha-backgrounds'; // don't change this
 const EXTENSIONS_CACHE_NAME = 'ugwisha-extensions'; // don't change this either
 const urlsToCache = [
