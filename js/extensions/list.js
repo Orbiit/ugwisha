9e830cd073038abbde81ff5e856b78a6bc7fda27
SheepTester <ussa.say@gmail.com>
2020-01-30 20:29:03 -0800

    Update club list last updated date

--- js/extensions/list.js
+++ js/extensions/list.js
@@ -7,7 +7,7 @@ window.UgwishaExtensions.register((() => {
     club: {
       name: 'Clubs',
       source: 'https://docs.google.com/spreadsheets/d/1HUaNWegOIk972lGweoSuNcXtfX7XuGBTQU-gcTsvD9s/',
-      sourceDate: new Date(2019, 8, 13),
+      sourceDate: new Date(2020, 0, 30),
       json: 'https://orbiit.github.io/gunn-web-app/json/clubs.json',
       subtext: ['room', 'day'],
       details: [
